f21a9b29dec61643f46fc53ceeeca5ce95d98e5f
arpitpandey01 <arpitpandey1505@gmail.com>
2025-08-03 20:17:55 +0530

    adding new resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -43,7 +43,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1PogzvrI7fGUWAC_7hBv4fXDJEnRB6m6S/view?usp=sharing"
+            href="https://drive.google.com/file/d/1mw8HH-D9doON-Tx3vv5HwAZGVn1igy0d/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block border border-[#a78bfa] text-[#a78bfa] font-semibold text-lg py-3 px-8 rounded-full hover:bg-[#6366f1]/10 hover:scale-105 transition duration-300"
